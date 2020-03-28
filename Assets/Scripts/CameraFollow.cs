9d39ef92cb36b328a4b26794ab2bf02853c35392
zhd407108459 <407108459@qq.com>
2020-03-29 01:59:06 +0800

    Update Camera Follow

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,14 +6,21 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform target;
     public float height = 10.0f;
+    public float size = 5.0f;
+    public bool onlySetSizeOnce = true;
 
     void Start()
     {
         Camera.main.transform.position = new Vector3(target.position.x, target.position.y + height, target.position.z);
+        Camera.main.orthographicSize = size;
     }
 
     void Update()
     {
         Camera.main.transform.position = new Vector3(target.position.x, target.position.y + height, target.position.z);
+        if (!onlySetSizeOnce)
+        {
+            Camera.main.orthographicSize = size;
+        }
     }
 }
