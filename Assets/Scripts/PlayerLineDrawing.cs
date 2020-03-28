5d9220705e63caaa2e43e47db9a507f948d4b6ae
zhd407108459 <407108459@qq.com>
2020-03-29 01:54:49 +0800

    Update Player Line Drawing

--- Assets/Scripts/PlayerLineDrawing.cs
+++ Assets/Scripts/PlayerLineDrawing.cs
@@ -11,12 +11,15 @@ public class PlayerLineDrawing : MonoBehaviour
     public Transform lineTextsContainer;
     public GameObject lineTextPrefab;
 
+    public Material showMaterial;
+    public Material warningMaterial;
     public float dangerDistance = 4.0f;
     public float escapeTime = 3.0f;
     public Slider timeSlider;
 
     private List<GameObject> lines = new List<GameObject>();
     private List<GameObject> lineTexts = new List<GameObject>();
+    private List<Transform> targets = new List<Transform>();
     private bool isTouching;
     private float touchingTime;
 
@@ -55,42 +58,60 @@ public class PlayerLineDrawing : MonoBehaviour
 
     private void DrawDistanceLines()
     {
-        for (int i = 0; i < lines.Count; i++)
-        {
-            Destroy(lines[i]);
-            Destroy(lineTexts[i]);
-            lines.RemoveAt(i);
-            lineTexts.RemoveAt(i);
-            i--;
-        }
+        targets.Clear();
         isTouching = false;
+
         Collider[] cos = Physics.OverlapSphere(transform.position, showDistance);
         for (int i = 0; i < cos.Length; i++)
         {
             if (cos[i].tag.Equals("NPC"))
             {
-                GameObject l = Instantiate(linePrefab);
-                LineRenderer lr = l.GetComponent<LineRenderer>();
-                if (lr != null)
-                {
-                    lr.SetPosition(0, transform.position);
-                    lr.SetPosition(1, cos[i].transform.position);
-                }
-                lines.Add(l);
-                Vector3 pos = (transform.position + cos[i].transform.position) / 2;
-                Vector3 screenPos = Camera.main.WorldToScreenPoint(pos);
-                GameObject t = Instantiate(lineTextPrefab, lineTextsContainer);
-                t.transform.position = screenPos;
-                if (t.GetComponent<Text>() != null)
-                {
-                    t.GetComponent<Text>().text = Vector3.Distance(transform.position, cos[i].transform.position).ToString("f2");
-                }
-                lineTexts.Add(t);
-                if (Vector3.Distance(transform.position, cos[i].transform.position) <= dangerDistance)
-                {
-                    isTouching = true;
-                }
+                targets.Add(cos[i].transform);
             }
         }
+
+        for (int i = 0; i < lines.Count; i++)
+        {
+            lines[i].SetActive(false);
+            lineTexts[i].SetActive(false);
+        }
+
+        for (int i = 0; i < targets.Count; i++)
+        {
+            if (i >= lines.Count)
+            {
+                GameObject line = Instantiate(linePrefab);
+                GameObject lineText = Instantiate(lineTextPrefab, lineTextsContainer);
+                lines.Add(line);
+                lineTexts.Add(lineText);
+            }
+            lines[i].SetActive(true);
+            lineTexts[i].SetActive(true);
+            LineRenderer lr = lines[i].GetComponent<LineRenderer>();
+            Text lt = lineTexts[i].GetComponent<Text>();
+            if (lr != null)
+            {
+                lr.SetPosition(0, transform.position);
+                lr.SetPosition(1, targets[i].transform.position);
+                if (Vector3.Distance(transform.position, targets[i].position) <= dangerDistance)
+                {
+                    lr.material = warningMaterial;
+                    isTouching = true;
+                }
+                else
+                {
+                    lr.material = showMaterial;
+                }
+
+            }
+            if (lt != null)
+            {
+                Vector3 pos = (transform.position + targets[i].position) / 2;
+                Vector3 screenPos = Camera.main.WorldToScreenPoint(pos);
+                lineTexts[i].transform.position = screenPos;
+                lt.text = Vector3.Distance(transform.position, targets[i].position).ToString("f2");
+            }
+        }
+
     }
 }
