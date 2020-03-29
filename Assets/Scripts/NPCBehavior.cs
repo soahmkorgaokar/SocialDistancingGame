017d7e0fb777e8f15687388bbf7df9242547b2ce
Mansoor Patan <1955mars@gmail.com>
2020-03-29 00:22:58 -0400

    NPC behavior added
    	- NPC prefab has Unity's NavMeshAgent now
    	- Follows social distancing with other NPC
    	- Randomnly picks one of the targets with 'NPCTarget' tag
    
    	How to use NPC in your scene?
    		- Place NPC prefabs in the scene (distribue them)
    		- Tag the destinations as 'NPCTarget'
    		- Need to bake the NavMesh in the scene (I can help if
    		you have never done before)

---
+++ Assets/Scripts/NPCBehavior.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCBehavior : MonoBehaviour
+{
+    /*
+     * NPCs also follow social distancing rules
+     * To change this distance, modify the below variable in the Unity Editor for NPC prefab
+     *      Nav Mesh Agent -> Obstacle Avoidance -> Radius (currenty set to 3 units)
+     */
+
+
+    public int agentID;
+    public int maxAgentsAtDestination = 3;
+    public float stopTime = 3.0f;
+    //public float speed;
+
+    static private int numAgents;
+    static private int[] destinationTrack;
+    static private bool setDestinationTrack = false;
+
+    private GameObject[] destinations;
+    private NavMeshAgent navAgent;
+    private int index = -1;
+    public bool hasStopped = true;
+    public float slowedDownSince = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numAgents++;
+        agentID = numAgents;
+
+        navAgent = GetComponent<NavMeshAgent>();
+        destinations = GameObject.FindGameObjectsWithTag("NPCTarget");
+        if(!setDestinationTrack)
+        {
+            destinationTrack = new int[destinations.Length];
+            setDestinationTrack = true;
+        }
+        PickRandomDestination();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!hasStopped)
+        {
+            if (Vector3.Distance(navAgent.transform.position, destinations[index].transform.position) < 1.0f)
+            {
+                hasStopped = true;
+                Invoke("PickRandomDestination", stopTime);
+            }
+            else if(navAgent.velocity.magnitude < 0.5f)
+            {
+                if(slowedDownSince >= stopTime)
+                {
+                    slowedDownSince = 0.0f;
+                    hasStopped = true;
+                    Invoke("PickRandomDestination", 0.5f);
+                }
+                else
+                {
+                    slowedDownSince += Time.deltaTime;
+                }
+            }
+        }
+        //speed = navAgent.velocity.magnitude;
+    }
+
+    void PickRandomDestination()
+    {
+        //Debug.Log("Agent ID: " + agentID + " Picking Random Destination");
+        int newIndex = Random.Range(0, destinations.Length);
+
+        if(newIndex != index && destinationTrack[newIndex] < maxAgentsAtDestination)
+        {
+            if(index > -1 && destinationTrack[index] > 0)
+                destinationTrack[index]--;
+            index = newIndex;
+            navAgent.destination = destinations[index].transform.position;
+            destinationTrack[index]++;
+            hasStopped = false;
+        }
+        else
+        {
+            PickRandomDestination();
+        }
+
+    }
+}
